ea08d751d365815ae599564f18e403a835b0572f
Black GitHub <creatorsavya@gmail.com>
2026-06-22 01:19:05 +0530

    Enhancement: Expand domain seeder database to 2300+ unique domains

--- scraper/domain_seeder.py
+++ scraper/domain_seeder.py
@@ -2,7 +2,7 @@
 scraper/domain_seeder.py
 
 Dynamic Domain Seeder & Career Page Resolver.
-Generates a comprehensive registry of ~2,500 Indian public sector, academic,
+Generates a comprehensive registry of 2,300+ Indian public sector, academic,
 PSU, and banking domains, and provides dynamic resolution of career pages.
 """
 
@@ -20,7 +20,111 @@ _STATE_CODES = [
     "up", "uk", "wb", "dl", "py", "ch", "an", "ld", "dd", "dn"
 ]
 
-# Major PSU domain list
+# 30 key state government departments/directorates
+_DEPARTMENTS = [
+    "education", "health", "pwd", "forest", "agriculture", "revenue", "transport", 
+    "wrd", "sports", "police", "finance", "coop", "excise", "tourism", "socialwelfare", 
+    "tribal", "urban", "rural", "industries", "planning", "animalhusbandry", "fisheries", 
+    "labour", "information", "law", "energy", "science", "technicaleducation", "highereducation", "home",
+    "food", "welfare", "housing", "water", "disaster"
+]
+
+# Comprehensive list of ~430 districts in India
+_DISTRICTS = [
+    # UP
+    "lucknow", "kanpur", "varanasi", "allahabad", "agra", "meerut", "ghaziabad", "bareilly", "aligarh", "moradabad", 
+    "saharanpur", "gorakhpur", "jhansi", "muzaffarnagar", "mathura", "ayodhya", "mirzapur", "firozabad", "raebareli", 
+    "sitapur", "hardoi", "lakhimpur", "barabanki", "unnao", "sultanpur", "amethi", "bahraich", "shravasti", "balrampur", 
+    "gonda", "basti", "amroha", "bijnor", "rampur", "sambhal", "pilibhit", "shahjahanpur", "kanpurdehat", "farrukhabad", 
+    "kannauj", "etawah", "auraiya", "jalaun", "hamirpur", "mahoba", "banda", "chitrakoot", "fatehpur", "pratapgarh", 
+    "kaushambi", "jaunpur", "ghazipur", "chandauli", "ballia", "mau", "deoria", "kushinagar", "maharajganj", "bhadohi", 
+    "sonbhadra", "lalitpur", "hapur", "shamli", "baghpat", "bulandshahr", "kasganj", "hathras", "etah", "mainpuri", "azamgarh",
+    # Maharashtra
+    "mumbai", "thane", "pune", "nagpur", "nashik", "aurangabad", "solapur", "amravati", "kolhapur", "sangli", "satara", 
+    "jalgaon", "dhule", "chandrapur", "latur", "akola", "parbhani", "buldhana", "yavatmal", "wardha", "bhandara", "gondia", 
+    "gadchiroli", "hingoli", "osmanabad", "beed", "nandurbar", "ratnagiri", "sindhudurg", "raigad", "palghar", "nanded", 
+    "jalna", "washim",
+    # Bihar
+    "patna", "gaya", "muzaffarpur", "bhagalpur", "darbhanga", "purnia", "arrah", "begusarai", "katihar", "munger", 
+    "chhapra", "saharsa", "sasaram", "hajipur", "siwan", "motihari", "bettiah", "jehanabad", "nawada", "nalanda", 
+    "buxar", "rohtas", "bhojpur", "vaishali", "saran", "samastipur", "madhubani", "sitamarhi", "sheohar", "araria", 
+    "kishanganj", "supaul", "madhepura", "khagaria", "jamui", "lakhisarai", "sheikhpura", "banka", "arwal",
+    # Gujarat
+    "ahmedabad", "surat", "vadodara", "rajkot", "bhavnagar", "jamnagar", "junagadh", "gandhinagar", "nadiad", "morbi", 
+    "surendranagar", "gandhidham", "veraval", "navsari", "bharuch", "anand", "porbandar", "godhra", "patan", "dahod", 
+    "amreli", "valsad", "vapi", "mehsana", "palanpur", "vyara", "ahwa", "himatnagar", "modasa", "chhotaudepur", "botad",
+    # Rajasthan
+    "jaipur", "jodhpur", "kota", "bikaner", "ajmer", "udaipur", "bhilwara", "alwar", "sikar", "sriganganagar", "pali", 
+    "hanumangarh", "tonk", "baran", "bundi", "churu", "dholpur", "jaisalmer", "jalore", "jhalawar", "jhunjhunu", "karauli", 
+    "nagaur", "pratapgarh", "rajsamand", "sawaimadhopur", "sirohi",
+    # MP
+    "bhopal", "indore", "jabalpur", "gwalior", "ujjain", "sagar", "dewas", "satna", "ratlam", "rewa", "murwara", "singrauli", 
+    "burhanpur", "khandwa", "bhind", "chhindwara", "guna", "shivpuri", "vidisha", "chhatarpur", "damoh", "mandsaur", 
+    "khargone", "neemuch", "hoshangabad", "itarsi", "sehore", "betul", "seoni", "balaghat", "mandla", "dindori", "shahdol", 
+    "anuppur", "umaria", "sidhi", "jhabua", "alirajpur", "dhar", "barwani", "shajapur", "agar-malwa", "rajgarh", "sheopur", 
+    "morena", "datia", "tikamgarh", "niwari", "panna", "katni", "narsinghpur", "harda", "raisen",
+    # Andhra
+    "visakhapatnam", "vijayawada", "guntur", "nellore", "kurnool", "kakinada", "kadapa", "tirupati", "anantapur", 
+    "eluru", "ongole", "nandyal", "machilipatnam", "adoni", "tenali", "proddatur", "chittoor", "hindupur", "bhimavaram", 
+    "madanapalle", "guntakal", "srikakulam", "vizianagaram", "amalapuram", "parvathipuram",
+    # Telangana
+    "hyderabad", "warangal", "nizamabad", "karimnagar", "khammam", "ramagundam", "mahabubnagar", "nalgonda", "adilabad", 
+    "suryapet", "miryalaguda", "jagtial", "mancherial", "kothagudem", "siricilla", "kamareddy", "siddipet", "wanaparthy", 
+    "gadwal", "narayanpet", "bhupalpally", "mulugu",
+    # Tamil Nadu
+    "chennai", "coimbatore", "madurai", "tiruchirappalli", "tiruppur", "salem", "erode", "vellore", "thoothukudi", 
+    "dindigul", "thanjavur", "ranipet", "sivakasi", "karur", "udagamandalam", "nagercoil", "kanchipuram", "tiruvannamalai", 
+    "cuddalore", "dharmapuri", "krishnagiri", "namakkal", "nilgiris", "perambalur", "pudukkottai", "ramanathapuram", 
+    "sivaganga", "theni", "thiruvallur", "thiruvarur", "tirunelveli", "tenkasi", "tirupathur", "villupuram", "virudhunagar",
+    # Karnataka
+    "bengaluru", "mysuru", "hubballi", "dharwad", "mangaluru", "belagavi", "kalaburagi", "davanagere", "ballari", 
+    "vijayapura", "shivamogga", "tumakuru", "raichur", "bidar", "hospet", "hassan", "gadag", "bagalkot", "kolar", 
+    "mandya", "chikmagalur", "chitradurga", "haveri", "yadgir", "ramanagara", "chamarajanagar", "udupi", "kodagu", 
+    "karwar", "koppal", "chikkaballapur",
+    # Kerala
+    "thiruvananthapuram", "kochi", "kozhikode", "kollam", "thrissur", "alappuzha", "palakkad", "kannur", "kottayam", 
+    "kasaragod", "pathanamthitta", "idukki", "wayanad", "malappuram",
+    # West Bengal
+    "kolkata", "howrah", "darjeeling", "kalimpong", "jalpaiguri", "alipurduar", "coochbehar", "malda", "murshidabad", 
+    "nadia", "purulia", "bankura", "birbhum", "hooghly", "midnapore", "kharagpur", "asansol", "durgapur", "siliguri", 
+    "bardhaman", "jhargram", "purvamedinipur", "paschimmedinipur",
+    # Punjab
+    "ludhiana", "amritsar", "jalandhar", "patiala", "bathinda", "mohali", "pathankot", "hoshiarpur", "batala", "moga", 
+    "phagwara", "firozpur", "muktsar", "barnala", "faridkot", "gurdaspur", "kapurthala", "mansa", "rupnagar", "sangrur", 
+    "tarntaran", "fazilka", "malerkotla",
+    # Haryana
+    "gurugram", "faridabad", "panipat", "ambala", "yamunanagar", "rohtak", "hisar", "karnal", "sonipat", "panchkula", 
+    "sirsa", "bhiwani", "bahadurgarh", "jind", "kaithal", "rewari", "palwal", "nuh", "fatehabad", "mahendragarh", 
+    "charkhidadri", "jhajjar",
+    # Odisha
+    "khordha", "cuttack", "ganjam", "bhadrak", "balasore", "mayurbhanj", "puri", "sambalpur", "rourkela", "sundargarh", 
+    "bolangir", "koraput", "rayagada", "kalahandi", "nawarangpur", "malkangiri", "kendrapada", "jajpur", "jagatsinghpur", 
+    "dhenkanal", "angul", "keonjhar", "nayagarh", "boudh", "subarnapur", "bargarh", "jharsuguda", "deogarh", "nuapada", "gajapati",
+    # J&K and North-East
+    "anantnag", "bandipora", "baramulla", "budgam", "doda", "ganderbal", "kathua", "kishtwar", "kulgam", "kupwara", 
+    "poonch", "pulwama", "ramban", "reasi", "samba", "shopian", "udhampur", "northgoa", "southgoa", "dhalai", "gomati", 
+    "khowai", "northtripura", "sepahijala", "southtripura", "unakoti", "westtripura", "bishnupur", "chandel", "churachandpur", 
+    "imphaleast", "imphalwest", "jiribam", "kakching", "kamjong", "kangpokpi", "noney", "pherzawl", "senapati", "tamenglong", 
+    "tengnoupal", "thoubal", "ukhrul", "eastgarohills", "eastjaintiahills", "eastkhasihills", "northgarohills", "ribhoi", 
+    "southgarohills", "southwestgarohills", "southwestkhasihills", "westgarohills", "westjaintiahills", "westkhasihills", 
+    "aizawl", "champhai", "kolasib", "lawngtlai", "lunglei", "mamit", "saiha", "serchhip", "hnahthial", "khawzawl", "saitual",
+    "chumuoukedima", "dimapur", "kiphire", "kohima", "longleng", "mokokchung", "mon", "niuland", "noklak", "peren", "phek", 
+    "shamator", "tseminyu", "tuensang", "wokha", "zunheboto", "gangtok", "gyalshing", "mangan", "namchi", "soreng", "pakyong",
+    "tawang", "westkameng", "eastkameng", "papumpare", "kurungkumey", "kraadaadi", "lowersubansiri", "uppersubansiri", 
+    "westsiang", "eastsiang", "siang", "uppersiang", "lowersiang", "lowerdibangvalley", "dibangvalley", "anjaw", "lohit", 
+    "namsai", "changlang", "tirap", "longding", "kamle", "leparada"
+]
+
+# Municipal Corporations / Urban Local Bodies
+_MUNICIPALITIES = [
+    "mcgm", "pmc", "nmmc", "kdmc", "mbmc", "vvcmc", "ulhasnagar", "bnmc", "smc_gj", "amc_gj", 
+    "vmc_gj", "rmc_gj", "jmc_rj", "jo_mc", "kmc_wb", "hmc_wb", "mcshimla", "mcg", "mcf", "mcc_ka", 
+    "bmrda", "hmrda", "cmda", "ghmc", "gvmc", "vuda", "vada", "kda", "jda", "uda", "ada", "gda", 
+    "udaipurmc", "kotamc", "bikanermc", "ajmermc", "bhilwaramc", "alwarmc", "sikarmc", "pnbmc", 
+    "dharmc", "gwalior-mc", "bhopal-mc", "indore-mc", "jabalpur-mc", "sagar-mc", "satna-mc", "rewa-mc"
+]
+
+# Central PSUs
 _PSU_DOMAINS = [
     "gail.co.in", "gailonline.com", "oil-india.com", "nalcoindia.com", "mazagondock.in",
     "bhel.com", "ongcindia.com", "sail.co.in", "ntpc.co.in", "powergrid.in", "iocl.com",
@@ -28,22 +132,28 @@ _PSU_DOMAINS = [
     "dic.gov.in", "npcilcareers.co.in", "rites.com", "dfccil.com", "bpcl.in", "pfcindia.com",
     "recl.co.in", "itiltd.in", "celindia.co.in", "nhpcindia.com", "grid-india.in",
     "hindustanpetroleum.com", "irctc.com", "concorindia.co.in", "engineersindia.com",
-    "gail.co.in", "hzlindia.com", "hzl.co.in", "balcoindia.com", "vizagsteel.com",
+    "hzlindia.com", "hzl.co.in", "balcoindia.com", "vizagsteel.com",
     "meconlimited.co.in", "hecltd.com", "kioclltd.in", "midhani-india.in", "bdl-india.in",
     "grse.in", "goashipyard.co.in", "hsl.gov.in", "cochinshipyard.in", "hmtindia.com",
     "nmdc.co.in", "nationalfertilizers.com", "rcfltd.com", "fact.co.in", "mfl.co.in"
 ]
 
-# Major Banking/Insurance domain list
+# Public Banks, Insurance companies, and Regional Rural Banks
 _BANK_DOMAINS = [
     "sbi.co.in", "pnbindia.in", "bankofbaroda.in", "canarabank.com", "unionbankofindia.co.in",
     "indianbank.in", "mahabank.in", "indianoverseasbank.in", "ucobank.com", "bankofindia.co.in",
     "centralbankofindia.co.in", "psbindia.com", "rbi.org.in", "sebi.gov.in", "nabard.org",
     "nhb.org.in", "eximbankindia.in", "ecgc.in", "ibps.in", "licindia.in", "gicofindia.com",
-    "nia.co.in", "nationalinsurance.nic.in", "orientalinsurance.org.in", "newindia.co.in"
+    "nia.co.in", "nationalinsurance.nic.in", "orientalinsurance.org.in", "newindia.co.in",
+    # Regional Rural Banks (RRBs)
+    "apgbank.in", "apgvbank.in", "agb.co.in", "aryavartbank-rrb.com", "bgvb.in", "brkgb.com",
+    "cgrgb.co.in", "karnatakagraminbank.com", "kvgbank.in", "keralagbank.sg", "mgbgrameen.in",
+    "mpgb.co.in", "meghalayagraminbank.co.in", "mizoramruralbank.co.in", "nagalandruralbank.co.in",
+    "ogb.co.in", "pgb.org.in", "sgbrrb.org", "utkalgrameenbank.co.in", "ubgb.in", "pbgb.org.in",
+    "prathamaupbank.com", "supgrrb.com"
 ]
 
-# 23 IITs, 31 NITs, 25 IIITs, 20 IIMs, and 54 Central Universities suffixes
+# Academic & Research Suffixes (IITs, NITs, IIITs, Universities)
 _ACADEMIC_SEEDS = [
     # IITs
     "iitb.ac.in", "iitd.ac.in", "iitkgp.ac.in", "iitm.ac.in", "iitk.ac.in", "iitr.ac.in",
@@ -54,28 +164,49 @@ _ACADEMIC_SEEDS = [
     # NITs
     "nitrkl.ac.in", "nitk.ac.in", "nits.ac.in", "nitc.ac.in", "nitw.ac.in", "nitt.edu",
     "nitp.ac.in", "nitj.ac.in", "nitkkr.ac.in", "nitdgp.ac.in", "nitsri.ac.in",
-    "nitap.ac.in", "nitmanipur.ac.in", "nitm.ac.in", "nitmz.ac.in", "nitnagaland.ac.in",
+    "nitmanipur.ac.in", "nitm.ac.in", "nitmz.ac.in", "nitnagaland.ac.in",
     "nitsikkim.ac.in", "nitpy.edu.in", "nitgoa.ac.in", "nitdelhi.ac.in", "nith.ac.in",
-    "nitrr.ac.in", "nitjsr.ac.in", "nituk.ac.in", "nitap.ac.in",
+    "nitrr.ac.in", "nitjsr.ac.in", "nituk.ac.in",
+    # IIITs
+    "iiitd.ac.in", "iiitb.ac.in", "iiitm.ac.in", "iiitg.ac.in", "iiith.ac.in", "iiitk.ac.in",
+    "iiitl.ac.in", "iiitp.ac.in", "iiits.ac.in", "iiitvadodara.ac.in", "iiitkottayam.ac.in",
+    "iiitdharwad.ac.in", "iiitkalyani.ac.in", "iiituna.ac.in", "iiitranchi.ac.in",
+    "iiitnagpur.ac.in", "iiitbhagalpur.ac.in", "iiitbhopal.ac.in", "iiitsurat.ac.in",
     # Universities
     "du.ac.in", "jnu.ac.in", "bhu.ac.in", "uohyd.ac.in", "amu.ac.in", "jmi.ac.in",
-    "curaj.ac.in", "tezu.ernet.in", "iisc.ac.in", "tifr.res.in", "csir.res.in"
+    "curaj.ac.in", "tezu.ernet.in", "iisc.ac.in", "tifr.res.in", "csir.res.in",
+    "manipal.edu", "bits-pilani.ac.in", "annauniv.edu", "vtu.ac.in", "jntu.ac.in",
+    "caluniv.ac.in", "mu.ac.in", "unom.ac.in", "unipune.ac.in"
+]
+
+# Key Ministries, Central Agencies, and Research Labs
+_MINISTRIES_LABS = [
+    "meity.gov.in", "education.gov.in", "defence.gov.in", "finmin.nic.in", "mha.gov.in",
+    "railways.gov.in", "isro.gov.in", "drdo.gov.in", "barc.gov.in", "dae.gov.in",
+    "dst.gov.in", "dbtindia.gov.in", "csir.res.in", "icmr.gov.in", "icar.org.in",
+    "sac.gov.in", "vssc.gov.in", "ursc.gov.in", "sdsc.gov.in", "iprc.gov.in",
+    "iist.ac.in", "nrsc.gov.in", "iirs.gov.in", "prl.res.in", "narl.gov.in",
+    "neist.res.in", "iict.res.in", "ncl-india.org", "nplindia.org", "ccmb.res.in",
+    "cdri.res.in", "iiim.res.in", "ihbt.res.in", "imtech.res.in", "nio.org",
+    "ngri.res.in", "neeri.res.in", "cgcri.res.in", "cecri.res.in", "clri.org"
 ]
 
 def generate_domains():
     """
-    Generates a full list of ~2,500 target domains across:
-      - Central/State PSCs and SSBs
-      - State Police and Municipalities
-      - Major CPSE PSUs
-      - Public Banks & Insurance
-      - Elite Academic & Research Institutions
+    Generates a registry of 2,300+ target domains across:
+      - Central/State PSCs and SSBs (180+ domains)
+      - State Department portals (1,080+ domains)
+      - District NIC portals (430+ domains)
+      - Municipal Corporations (50+ domains)
+      - Central and State PSUs (160+ domains)
+      - Public Sector Banks & Regional Rural Banks (50+ domains)
+      - Elite Academic & Research Institutions (80+ domains)
+      - Ministries & Research Labs (40+ domains)
     """
     domains = {}
 
-    # 1. State PSC & Staff Selection Board combinations
+    # 1. State level portals (PSC, SSB, Police, Gov)
     for code in _STATE_CODES:
-        # e.g., uppsc.up.nic.in, mpsc.gov.in, rpsc.rajasthan.gov.in
         domains[f"{code}psc"] = {
             "name": f"{code.upper()} PSC (State Commission)",
             "url": f"https://{code}psc.gov.in"
@@ -88,13 +219,37 @@ def generate_domains():
             "name": f"{code.upper()} Police Recruitment",
             "url": f"https://{code}police.gov.in"
         }
-        # Nested patterns
         domains[f"{code}_gov"] = {
             "name": f"{code.upper()} Government Portal",
             "url": f"https://www.nic.in/{code}"
         }
 
-    # 2. PSUs
+    # 2. State departments subdomains (combining states * depts)
+    for code in _STATE_CODES:
+        for dept in _DEPARTMENTS:
+            key = f"{code}_{dept}"
+            domains[key] = {
+                "name": f"{code.upper()} {dept.title()} Department",
+                "url": f"https://{dept}.{code}.gov.in"
+            }
+
+    # 3. Districts (NIC Portals)
+    for dist in _DISTRICTS:
+        key = f"dist_{dist}"
+        domains[key] = {
+            "name": f"{dist.title()} District Portal",
+            "url": f"https://{dist}.nic.in"
+        }
+
+    # 4. Municipal Corporations
+    for muni in _MUNICIPALITIES:
+        key = f"muni_{muni}"
+        domains[key] = {
+            "name": f"{muni.upper().replace('_', ' ')} Municipal Corporation",
+            "url": f"https://www.{muni}.org"
+        }
+
+    # 5. PSUs
     for dom in _PSU_DOMAINS:
         key = dom.split(".")[0]
         domains[key] = {
@@ -102,7 +257,22 @@ def generate_domains():
             "url": f"https://www.{dom}"
         }
 
-    # 3. Banks
+    # 6. State PSUs (rtc, transco, genco)
+    for code in _STATE_CODES:
+        domains[f"{code}_transco"] = {
+            "name": f"{code.upper()} Transmission Corp (State PSU)",
+            "url": f"https://{code}transco.co.in"
+        }
+        domains[f"{code}_genco"] = {
+            "name": f"{code.upper()} Power Generation Corp (State PSU)",
+            "url": f"https://{code}genco.co.in"
+        }
+        domains[f"{code}_rtc"] = {
+            "name": f"{code.upper()} Road Transport Corp (State PSU)",
+            "url": f"https://{code}rtc.in"
+        }
+
+    # 7. Banks
     for dom in _BANK_DOMAINS:
         key = dom.split(".")[0]
         domains[f"bank_{key}"] = {
@@ -110,11 +280,19 @@ def generate_domains():
             "url": f"https://www.{dom}"
         }
 
-    # 4. Academic
+    # 8. Academic
     for dom in _ACADEMIC_SEEDS:
         key = dom.split(".")[0]
         domains[key] = {
             "name": f"{key.upper()} (Elite Academic/Research)",
+            "url": f"https://www.{dom}"
+        }
+
+    # 9. Ministries & Labs
+    for dom in _MINISTRIES_LABS:
+        key = dom.split(".")[0]
+        domains[f"lab_{key}"] = {
+            "name": f"{key.upper()} (Ministry/Research Lab)",
             "url": f"https://www.{dom}"
         }
 
